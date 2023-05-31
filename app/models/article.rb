586f63e99cbdb0d10d85c1866bae464bec320b09
Gage <gage@rubyshore.com>
2023-05-31 13:41:41 -0500

    added an important comment to the article.rb model for learning purposes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-
 class Article < ApplicationRecord
 
 end
 
+# you can run the rails console with 'rails console' command and then access these
+# classes. for instance run Article.all and it will generate a SQL command to
+# retrieve all from the database. pretty neat
