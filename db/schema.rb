ea2ccfbd3e4be0546adb232e97a5491d00d33848
Gage <gage@rubyshore.com>
2023-05-31 13:33:07 -0500

    new migration file to add description:text to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_181658) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_182839) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
