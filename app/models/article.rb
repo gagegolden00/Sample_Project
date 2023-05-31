ea47b5b5ffdee352c0763605b81d7d34dbcccc91
Gage <gage@rubyshore.com>
2023-05-31 14:48:57 -0500

    added more notes & input validation in models/article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
-
+  
+  # title validation
+  validates :title, presence: true, length: {minimum: 6, maximum: 50}
+  
+  # description validation
+  validates :description, presence: true, length: {minimum: 10, maximum: 500}
+  
 end
